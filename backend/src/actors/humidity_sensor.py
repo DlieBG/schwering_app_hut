e34307ecf2ce6c5b429fa8492df39e638f590288
Benedikt Schwering <mail@benedikt-schwering.de>
2024-06-16 19:48:30 +0200

    feat(humidity): add state command response to backend

--- backend/src/actors/humidity_sensor.py
+++ backend/src/actors/humidity_sensor.py
@@ -13,10 +13,26 @@ class HumiditySensor():
         if message.topic == f'{self.mqtt_prefix}/status/{self.mqtt_suffix}':
             self.humidity = json.loads(message.payload).get('rh')
             self.__invoke(self.__updates)
+
+        if message.topic == f'{self.mqtt_prefix}/command/{self.mqtt_suffix}':
+            if message.payload.decode('utf-8') == 'status_update':
+                self.__status_update()
     
     def __invoke(self, funcs):
         for func in funcs:
             func(self.parent, self.humidity)
+    
+    def __send_status(self, status: dict):
+        self.parent.mqtt_client.publish(
+            topic=f'{self.mqtt_prefix}/status/{self.mqtt_suffix}',
+            payload=json.dumps(status),
+        )
+
+    def __status_update(self):
+        if self.humidity is not None:
+            self.__send_status({
+                'rh': self.humidity,
+            })
     
     def update(self, func):
         self.__updates.append(func)
